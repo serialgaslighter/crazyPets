ec2f5871b4872d8f6e92a41ce00c6aeb36d64f40
Fabio <fabioresnik@dci-student.org>
2024-07-11 09:45:03 +0200

    fabios first commit on fabios branch

---
+++ fabioCrazyPets.js
@@ -0,0 +1,15 @@
+const user = {
+    name: "fabius",
+    art: "Zwerg",
+    alter: 127,
+    farbe: "dunkler als schwarz",
+    faehigkeit: "Zwergisch sprechen",
+    nahrung: "Drachenfleisch"
+}
+
+function PetArray(user){
+    let CrazyPetArray = [];
+    CrazyPetArray.push(user);
+    return CrazyPetArray;
+}
+console.log(CrazyPetArray(user));
