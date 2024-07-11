946d644df52a290fb949f1444440111eced55110
Oguz Sezgin <oguz.sezgin@dci-student.org>
2024-07-11 09:56:30 +0200

    function export

---
+++ sezo's>crazyPet.js
@@ -0,0 +1,15 @@
+const sezoCrazyPet = {
+    name: "Bello",
+    art: "Hund",
+    alter: 3,
+    farbe: "braun",
+    faehigkeit: "Super-Schnüffler",
+    nahrung: "Hundefutter"
+};
+
+console.log(sezoCrazyPet);
+
+export function addSezoCrazyPet(petArray) {
+    petArray.push(sezoCrazyPet);
+    return petArray;
+}
