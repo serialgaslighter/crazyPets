72e425b0ee383d12feef2e6718c1805da9006ee4
Fabio <fabioresnik@dci-student.org>
2024-07-11 15:00:38 +0200

    fabios correction with thomas on main branch

--- fabioCrazyPets.js
+++ fabioCrazyPets.js
@@ -1,16 +1,15 @@
-const user = {
-    name: "fabius",
-    art: "Zwerg",
-    alter: 127,
-    farbe: "dunkler als schwarz",
-    faehigkeit: "Zwergisch sprechen",
-    nahrung: "Drachenfleisch"
+const fabiosPets = {
+  name: "fabius",
+  art: "Zwerg",
+  alter: 127,
+  farbe: "dunkler als schwarz",
+  faehigkeit: "Zwergisch sprechen",
+  nahrung: "Drachenfleisch",
+};
+
+export function addfabioCrazyPets(user) {
+  user.push(fabiosPets);
+  return user;
 }
 
-export function addfabioCrazyPets(user){
-    let CrazyPetArray = [];
-    CrazyPetArray.push(user);
-    return CrazyPetArray;
-}
-
-console.log(addfabioCrazyPets(user));
+//console.log(addfabioCrazyPets(user));
